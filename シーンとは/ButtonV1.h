729637d6193b946a79d1d1060a1f1f39892ff24b
Mya-Mya <interleukin413@gmail.com>
2018-09-15 19:21:01 +0900

    Folder Deleted

--- シーンとは/ButtonV1.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-//  ButtonV1(左上ｘ          左上ｙ          右下ｘ          右下ｙ          表示文字           文字色                     文字サイズ       ボタン標準色              カーソル合わせ時の色      ボタンクリック時の色
-int ButtonV1(signed int LTx, signed int LTy, signed int RBx, signed int RBy, const TCHAR *name, unsigned int letter_color, int letter_size, unsigned int normal_color, unsigned int cursor_color, unsigned int clicking_color);
